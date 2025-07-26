97a6a528ca3ddbbc7aa65171a261bc80ab3bfe21
Thao <nguyenthao.france@gmail.com>
2025-07-26 06:33:35 +0200

    V28

--- 01-react-vite-hoidanit-starter-master/src/App.jsx
+++ 01-react-vite-hoidanit-starter-master/src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 // () => {}
+//component = html + css + js
+const MyComponent = () => {
+  return (
+    <div>Eric & hoidanit</div>
+  )
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
-
-  const myFunction = () => {
-    console.log(">>> RUNNING");
-  }
-  myFunction();
 
   return (
     <>
@@ -23,6 +26,7 @@ const App = () => {
         </a>
       </div>
       <h1>React + Vite</h1>
+      <MyComponent></MyComponent>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
