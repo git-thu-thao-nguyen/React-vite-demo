26b61cdd09d9a2f57c9ddff249deec99845dff01
Thao <nguyenthao.france@gmail.com>
2025-08-01 14:43:08 +0200

    V30

--- 01-react-vite-hoidanit-starter-master/src/components/learn/MyComponent.jsx
+++ 01-react-vite-hoidanit-starter-master/src/components/learn/MyComponent.jsx
@@ -4,8 +4,23 @@
 import './style.css';
 
 const MyComponent = () => {
+
+    // const hoidanit = "eric 1"; //String
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefined; //undefined
+    // const hoidanit = null; //null
+    // const hoidanit = [1,2,3]; //array
+    const hoidanit = { //objet
+        name: "hoidanit",
+        age: 25
+    }
+
     return (
         <>
+            {/* <div>{hoidanit.name}</div> */}
+            <div>{JSON.stringify(hoidanit)}</div>
+            <div>{console.log("ERIC IN CONSOLE")}</div> 
             <div>Eric & hoidanit</div> 
             <div className="child" style={{borderRadius: "10px"}}>child</div>
         </>
